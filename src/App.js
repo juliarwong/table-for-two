c8c6645b4471774b62d3a386e2ced832222d0ac8
juliarwong <wong.julia.r@gmail.com>
2023-05-24 22:00:11 -0400

    added the function to display data from the yelp api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect } from "react";
+
 function App() {
+  const fetchRestaurants = async () => {
+    const url = new URL("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search");
+    const params = new URLSearchParams({
+      location: "toronto",
+      term: "restaurants"
+    });
+
+    url.search = params;
+
+    const headers = {
+      Authorization: "Bearer 5-p-LWD5Yk5hoDqXSEZ0ysvtzxvM2OdMgkjt-itYb98PfN6ja_M4VRjtO6G0bM8SscNypvSIdQ7EowKRkVnWHuNRQRIfftwNPaghazmR0dnUxSRpStq7U5S5bYNmZHYx" 
+    };
+
+    try {
+      const response = await fetch(url, {
+        headers: headers
+      });
+      const data = await response.json();
+      // Process the data or perform any other actions based on the API response
+      console.log(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRestaurants();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="wrapper">
+      <h1>Table for Two!</h1>
+    </main>
   );
 }
 
