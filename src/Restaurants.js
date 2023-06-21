def4cbfaa02f626f82d05e2ee20998a9e77ef88d
juliarwong <wong.julia.r@gmail.com>
2023-06-21 15:48:47 -0400

    centered h3 elements on the page and fixed text displaying on page load

--- src/Restaurants.js
+++ src/Restaurants.js
@@ -1,8 +1,31 @@
+// import React from 'react';
+
+// function Restaurants({ restaurants, inputCity }) {
+//     if (!restaurants || restaurants.length === 0) {
+//         return <p>No restaurants found</p>;
+//     }
+
+//     return (
+//         <div>
+//         <h2>Top Restaurants in {inputCity}</h2>
+//         {restaurants.map((restaurant) => (
+//             <div key={restaurant.id}>
+//             <h3>{restaurant.name}</h3>
+//             <p>{restaurant.location.address1}</p>
+//             <p>{restaurant.phone}</p>
+//             </div>
+//         ))}
+//         </div>
+//     );
+// }
+
+// export default Restaurants;
+
 import React from 'react';
 
 function Restaurants({ restaurants, inputCity }) {
     if (!restaurants || restaurants.length === 0) {
-        return <p>No restaurants found</p>;
+        return null; // Return null if no restaurants are found
     }
 
     return (
