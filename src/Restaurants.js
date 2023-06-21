ca25520ca24274962317672f0208cdb8e166f112
juliarwong <wong.julia.r@gmail.com>
2023-06-21 16:46:32 -0400

    fixed spacing of results and added media queries

--- src/Restaurants.js
+++ src/Restaurants.js
@@ -1,26 +1,3 @@
-// import React from 'react';
-
-// function Restaurants({ restaurants, inputCity }) {
-//     if (!restaurants || restaurants.length === 0) {
-//         return <p>No restaurants found</p>;
-//     }
-
-//     return (
-//         <div>
-//         <h2>Top Restaurants in {inputCity}</h2>
-//         {restaurants.map((restaurant) => (
-//             <div key={restaurant.id}>
-//             <h3>{restaurant.name}</h3>
-//             <p>{restaurant.location.address1}</p>
-//             <p>{restaurant.phone}</p>
-//             </div>
-//         ))}
-//         </div>
-//     );
-// }
-
-// export default Restaurants;
-
 import React from 'react';
 
 function Restaurants({ restaurants, inputCity }) {
